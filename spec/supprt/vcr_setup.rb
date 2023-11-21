d4d317a2d6f8da69141e7f556d4164e142637744
Daniel Cooper <dan.cooper@berkeley.edu>
2023-11-20 20:21:53 -0800

    added working VCR

--- spec/supprt/vcr_setup.rb
+++
@@ -1,8 +0,0 @@
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-  config.filter_sensitive_data('<GOOGLE_CIVIC_API_KEY>') { Rails.application.credentials[:GOOGLE_API_KEY] }
-end
