e7022c8b00dd5e00d537649d3eaa0c490e86d6c6
Daniel Cooper <dan.cooper@berkeley.edu>
2023-11-22 20:29:15 -0800

    updated rspec test to check if rep is present

--- spec/controllers/representatives_controller_spec.rb
+++ spec/controllers/representatives_controller_spec.rb
@@ -5,7 +5,7 @@ require 'google/apis/civicinfo_v2'
 
 describe RepresentativesController, type: :request do
   before do
-    gavin = {
+    mickey = {
       name:            'Mickey Mouse',
       street:          '110th 1675 North Buena Vista Drive',
       city:            'The Magic Kingdom',
@@ -15,11 +15,12 @@ describe RepresentativesController, type: :request do
       photo_url:       'https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png?20220210025314',
       title:           'Mouse'
     }
-    @gavin = Representative.create(gavin)
+    @mickey = Representative.create(mickey)
   end
 
   it 'loads the correct representative' do
     get '/representatives/1'
     expect(response).to render_template(:show)
+    expect(response.body).to include('Mickey Mouse')
   end
 end
