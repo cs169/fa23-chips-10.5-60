a798ef8455dea6cf76e60f036c5bb75ea48614da
Yunshen Song <hsong159@berkeley.edu>
2023-12-01 07:20:22 +0000

    fixed haml-lint

---
+++ app/helpers/campaign_finance_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module CampaignFinanceHelper
+  def campaign_finance_categories
+    ['candidate-loan', 'contribution-total', 'debts-owed', 
+     'disbursements-total', 'end-cash', 'individual-total', 
+     'pac-total', 'receipts-total', 'refund-total']
+  end
+end
